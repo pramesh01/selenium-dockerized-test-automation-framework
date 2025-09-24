53da951988d1dfc20aa811ab61ac0894da7b9198
pramesh01 <pramesh.cs@gmail.com>
2025-09-25 00:53:40 +0530

    updating Readme.md file

--- src/test/java/pageclasses/OrderSummaryPage.java
+++ src/test/java/pageclasses/OrderSummaryPage.java
@@ -1,8 +1,12 @@
 package pageclasses;
 
+import java.time.Duration;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.BrowserUtilities;
 
 
@@ -14,7 +18,24 @@ public class OrderSummaryPage extends BrowserUtilities {
 	}
 
 	public OrderConfirmationPage confirmingOrder() {
-		click(I_CONFIRM_MY_ORDER_BUTTON_LOCATOR);
+		//click(I_CONFIRM_MY_ORDER_BUTTON_LOCATOR);
+		WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+
+	    try {
+	        // Wait until button is clickable
+	        WebElement confirmButton = wait.until(
+	            ExpectedConditions.elementToBeClickable(I_CONFIRM_MY_ORDER_BUTTON_LOCATOR)
+	        );
+
+	        // Try normal click
+	        confirmButton.click();
+	    } catch (Exception e) {
+	        System.out.println("Normal click failed, trying JS click...");
+
+	        // Fallback to JavaScript click
+	        WebElement confirmButton = getDriver().findElement(I_CONFIRM_MY_ORDER_BUTTON_LOCATOR);
+	        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", confirmButton);
+	    }
 		return new OrderConfirmationPage(getDriver());
 	}
 
